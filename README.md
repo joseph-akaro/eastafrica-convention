b15d0182a95feb1db3517360c00a875b55f48fc9
Joseph Akaro <josphmmakaro@gmail.com>
2025-07-12 20:21:31 +0300

    Frontend and Backend Done

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# eastafrica-convention
-This is a private repository for AA East Africa Convention 2025, It's function as a landing page, store backend system and payment system.
+# AA EAST AFRICA CONVENTION 2025
---
+++ app/auth/page.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div>Auth</div>
+  )
+}
+
+export default Auth
+
+
+
+const LogIn = () => {
+    return(
+        <div>
+            Login
+        </div>
+    )
+}
+
+const LogOut = () => {
+    return(
+        <div>
+            Logout
+        </div>
+    )
+}
